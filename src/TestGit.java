5860667dd8c373a96279bfb356b43db5d2308cfe
yevheniinokhrin <233315@edu.p.lodz.pl>
2022-06-09 16:55:56 +0200

    commit1

--- src/TestGit.java
+++ src/TestGit.java
@@ -3,5 +3,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("sdasdas");
         System.out.println("sssss");
+        System.out.println("3");
     }
 }
